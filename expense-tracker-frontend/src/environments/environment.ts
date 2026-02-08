942012f0881aa8d70099d01c42b47aecc6ecb678
THARUN-JULAKANTI <julakantitharun@gmail.com>
2026-02-08 15:01:37 +0530

    Initial commit: expense tracker frontend, backend, docker setup

---
+++ expense-tracker-frontend/src/environments/environment.ts
@@ -0,0 +1,8 @@
+// export const environment = {
+//   apiUrl: 'http://localhost:8081/api'
+// };
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://expensetracker-backend-12345.azurewebsites.net/api'
+};
---
+++ expensetracker/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO category (category_name) VALUES
+('Food'),
+('Travel'),
+('Rent'),
+('Groceries'),
+('Office Supplies'),
+('Client Expenses'),
+('Entertainment'),
+('EMI'),
+('Others');
